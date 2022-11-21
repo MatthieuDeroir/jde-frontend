978913ce6ad285412a29ddbc4cb771d3cd0ac02d
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-11-21 10:40:29 +0100

    update ip

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 
 class AuthService {
-     login(username, password) {
-         return axios.post("http://localhost:4000/auth/signin", {
-                username,
-                password
-            }).then(response => {
-                if (response.data.accessToken) {
-                    localStorage.setItem("user", JSON.stringify(response.data));
-                }
-               return response.data;
-            });
+    login(username, password) {
+        return axios.post("http://192.168.1.42:4000/auth/signin", {
+            username,
+            password
+        }).then(response => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+            return response.data;
+        });
     }
     logout() {
         localStorage.removeItem("user");
     }
     register(username, password) {
-        return axios.post("http://localhost:4000/auth/signup", {
+        return axios.post("http://192.168.1.42:4000/auth/signup", {
             username,
             password
         });
     }
 
-    updateUser(_id, role){
+    updateUser(_id, role) {
         let roles = [];
         roles[0] = role;
-        return axios.put("http://localhost:4000/user/" + _id, {
+        return axios.put("http://192.168.1.42:4000/user/" + _id, {
             roles
         });
     }
